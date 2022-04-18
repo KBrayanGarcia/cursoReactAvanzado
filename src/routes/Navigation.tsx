d4a6f791ee3e71a2703bc3d608fda6a4789b83fc
Brayan Garcia <brayangapkb@gmail.com>
2022-04-17 22:06:13 -0500

    Seguir desarollando

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { BrowserRouter, Route, Routes, NavLink, Navigate } from 'react-router-dom';
+
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import Logo from "../logo.svg";
+
+const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={Logo} alt="Logo" />
+                    <ul>
+                        <li>
+                            <NavLink to="/lazy1" className={({isActive}) => isActive ? "nav-active" : "" }>Lazy 1</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/lazy2" className={({isActive}) => isActive ? "nav-active" : "" }>Lazy 2</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/lazy3" className={({isActive}) => isActive ? "nav-active" : "" }>Lazy 3</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="lazy1" element={<LazyPage1/>} />
+                    <Route path="lazy2" element={<LazyPage2/>} />
+                    <Route path="lazy3" element={<LazyPage3/>} />
+                    {/* Replace para que no pueda regresar */}
+                    <Route path="/*" element={<Navigate to={"/lazy1"} replace />} />
+                </Routes>
+
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default Navigation;
